bbf5aaea90d57bfff534902c203901b90ac724d7
salehah <salehah737@outlook.com>
2025-04-19 12:43:39 +0000

    Finalize changes for PR

--- backend/apps/ecommerce/apps.py
+++ backend/apps/ecommerce/apps.py
@@ -1,6 +1,6 @@
+# backend/apps/ecommerce/apps.py
 from django.apps import AppConfig
 
-
 class EcommerceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.BigAutoField"  # Added underscore
     name = "ecommerce"
